66c23bfc521cca017b602a7f7850ed0dee083076
Amirkhan <amirhan75@gmail.com>
2023-01-24 17:57:39 +0600

    new

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Давайте выясним, сегодня у Вас сегодня выходной или нет? Какой сегодня день недели? (напишите номер дня): ");
+int day = int.Parse(Console.ReadLine());
+switch (day) 
+{
+    case 1: Console.WriteLine ("Понедельник"); 
+    break;
+    case 2: Console.WriteLine ("Вторник"); 
+    break;
+    case 3: Console.WriteLine ("Среда"); 
+    break;
+    case 4: Console.WriteLine ("Четверг"); 
+    break;
+    case 5: Console.WriteLine ("Пятница"); 
+    break;
+    case 6: Console.WriteLine ("Суббота");
+    break;
+    case 7: Console.WriteLine ("Воскресенье"); 
+    break;
+}
+    if (day == 6 || day == 7) 
+    {
+        Console.WriteLine("Сегодня у Вас выходной, отдыхайте.");
+        }
+    else {
+        Console.WriteLine("Сегодня у Вас рабочий день, идите на работу.");
+        }
---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+if ((number1 % number2) == 0)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else{
+    Console.WriteLine("первое число не явлется");
+}
